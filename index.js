1d463c59d57cd7935d8e38213376ccf533c85eb8
Aaron Storck <aaronstorck@gmail.com>
2015-06-13 19:01:32 -0400

    Refactored SuperJS to Ouro.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ YSS'      YSSP~YSSY    S*S           YSSP  S*S    SSS  YSSY    YSS'
                        SP                  SP
                        Y                   Y
 
-                  -= Redis Client for SuperJS =-
+                  -= Redis Client for Ouro =-
  */
 
 module.exports.Redis = require('./lib/redis/class');
